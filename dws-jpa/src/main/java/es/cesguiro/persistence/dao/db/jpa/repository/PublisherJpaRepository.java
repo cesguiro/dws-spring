18d288ee559e481394cfbbbe41f6095067d844a0
César Guijarro <cesguiro@gmail.com>
2024-12-18 13:20:52 +0100

    fixed: impl jpa folder name

--- dws-jpa/src/main/java/es/cesguiro/persistence/dao/db/jpa/repository/PublisherJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package es.cesguiro.persistence.dao.db.jpa.repository;
-
-import es.cesguiro.persistence.dao.db.jpa.entity.PublisherEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublisherJpaRepository extends JpaRepository<PublisherEntity, Long> {
-}
